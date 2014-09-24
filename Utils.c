4605deacad1988c918952398b50cfe9be3f4ec50
Spl3en <spl3en.contact@gmail.com>
2014-09-24 03:42:28 +0200

    Moar utilities

--- Utils.c
+++ Utils.c
@@ -1,2 +1,23 @@
 #include "Utils.h"
 
+void *
+malloc_zero (size_t size)
+{
+	void *memory = malloc(size);
+	memset(memory, 0, size);
+
+	return memory;
+}
+
+
+bool
+str_is_in_array (char *str, char **array, int arraySize)
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		if (!strcmp(str, array[i]))
+			return true;
+	}
+
+	return false;
+}
--- Utils.h
+++ Utils.h
@@ -1,4 +1,57 @@
 #pragma once
 
+#include <time.h>
+#include <stdio.h>
+
+
+
+// ----- Type size -----
 #define sizeof_array(array) \
     ((int)(sizeof(array) / sizeof(*(array))))
+
+
+
+// ----- Boolean -----
+#ifndef BOOL
+#ifdef WIN32
+#include <windows.h>
+#else
+#define BOOL char
+#endif // WIN32
+#endif // BOOL
+
+#ifndef bool
+#define bool BOOL
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif // TRUE
+
+#ifndef false
+#define false (!(TRUE))
+#endif
+
+#ifndef true
+#define true TRUE
+#endif
+
+#ifndef false
+#define false FALSE
+#endif
+
+
+bool
+is_in_array (int value, int *array, int size);
+
+int
+str_is_in_array (char *value, char **array, int size);
+
+void *
+malloc_zero (size_t size);
+
+time_t
+get_now (void);
+
+char *
+get_now_str (void);
